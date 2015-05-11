929c0f9c0097dc4954def6e88846a044ee4f55bc
Junioramaral <junioramaral@JuniorAmaral.local>
2015-05-11 13:03:31 -0300

    Verify Startup Date Servers
    Run Script StartupDate.py

---
+++ StartupDate.sh
@@ -0,0 +1,4 @@
+HOME_MS=/home/oracle/ilegra/restart
+JAVA_HOME=/u01/Middleware/jdk1.7.0_51
+
+$JAVA_HOME/bin/java -cp /u01/Middleware/Oracle_Home/wlserver/server/lib/weblogic.jar weblogic.WLST StartupDate-1.0.py
